d639ab3a793fac5a7678083b13bbfd78c6b7665b
Naman <naman0298@gmail.com>
2026-03-13 10:29:51 +0530

    minor changes

--- 12th March 2026/tests/task17.spec.ts
+++ 12th March 2026/tests/task17.spec.ts
@@ -1,39 +1,17 @@
 import { test, expect } from "@playwright/test";
 
-test("Handle multiple browser tabs", async ({ page, context }) => {
-
-  // open amazon
+test("task 17", async ({ page }) => {
   await page.goto("https://www.amazon.in");
+  let search = page.locator("input#twotabsearchtextbox");
+  await search.fill("samsung mobile");
+  await search.press("Enter");
+  let firstProduct = page.locator("//img[@class='s-image']").first();
 
-  // search product
-  await page.getByPlaceholder("Search Amazon.in").fill("Samsung Mobile");
-  await page.keyboard.press("Enter");
+  let [page2] = await Promise.all([page.waitForEvent("popup"), firstProduct.click()]);
+  let productName = await page2.locator('//span[@id="productTitle"]');
+  await expect(productName).toBeVisible();
+  console.log(await productName.textContent());
   await page.screenshot({ path: 'screenshot/task17.png', fullPage: true });
 
-  // wait for search results
-  await page.waitForSelector('[data-component-type="s-search-result"]');
-
-  // store first product
-  const firstProduct = page.locator('[data-component-type="s-search-result"]').first();
-
-  // capture new tab
-  const [newTab] = await Promise.all([
-    context.waitForEvent("page"),
-    firstProduct.click()
-  ]);
-
-  // wait for product page
-  await newTab.waitForLoadState();
-
-  // get product title
-  const title = newTab.locator("#productTitle");
-
-  console.log(await title.textContent());
-
-  await expect(title).toBeVisible();
-
-  await newTab.close();
-
-  await page.bringToFront();
-
+  await page2.close();
 });
